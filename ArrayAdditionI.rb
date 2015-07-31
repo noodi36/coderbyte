0794f77bacb200ba088687732e2d9dab53e874b2
noodi36 <noodi306@gmail.com>
2015-07-31 17:04:15 +0900

    Create ArrayAdditionI.rb

---
+++ ArrayAdditionI.rb
@@ -0,0 +1,38 @@
+def ArrayAdditionI(arr)
+
+ a=arr.max
+  b=arr.reduce(:+)
+  c=a*2
+  d="true"
+  e="false"
+
+   if b==c
+    return d
+  else
+    return e
+  end
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+ArrayAdditionI(STDIN.gets)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
