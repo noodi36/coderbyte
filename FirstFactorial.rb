e1af3a3be72bb5efb5337e2f425084738a3a33ec
noodi36 <noodi306@gmail.com>
2015-07-31 14:17:52 +0900

    Create FirstFactorial.rb

---
+++ FirstFactorial.rb
@@ -0,0 +1,30 @@
+def FirstFactorial(num)
+	a=1
+  1.upto(num) {|x|a=a*x}
+  # code goes here
+  return a
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+FirstFactorial(STDIN.gets)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
